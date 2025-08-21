c327a9825515e4b7e5a95408eb55d85148a45da3
Daniel Moffett <moffd234@gmail.com>
2025-08-21 16:57:33 -0400

    Refactored import

--- lambda/intents/generate_fact_intent.py
+++ lambda/intents/generate_fact_intent.py
@@ -3,22 +3,22 @@ import ask_sdk_core.utils as ask_utils
 from ask_sdk_core.handler_input import HandlerInput
 from ask_sdk_model import Response
 import random
-import importlib
+from ..unfun_facts import unfun_facts
 
-unfun_facts = importlib.import_module('lambda.unfun_facts').unfun_facts
 
 class GenerateFactIntentHandler(AbstractRequestHandler):
     """Handler for Generate Fact Intent."""
+
     def can_handle(self, handler_input: HandlerInput) -> bool:
         return ask_utils.is_intent_name("GenerateFactIntent")(handler_input)
 
     def handle(self, handler_input: HandlerInput) -> Response:
-        fact = random.choice(unfun_facts)
+        fact = random.choice(unfun_facts.unfun_facts)
         speak_output = fact
 
         return (
             handler_input.response_builder
-                .speak(speak_output)
-                .ask("Would you like to hear another unfun fact?")
-                .response
+            .speak(speak_output)
+            .ask("Would you like to hear another unfun fact?")
+            .response
         )
